abb599a4088a2c1b4a6a58ccc61d2dd97c5015df
Manojnahak7 <159554819+Manojnahak7@users.noreply.github.com>
2024-08-30 13:30:36 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -6,22 +6,42 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
 const usersController = require("../controllers/users.js");
 const { saveRedirectUrl } = require("../middleware.js");
+const { storage } = require("../cloudConfig"); 
+const User = require("../models/user"); 
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "../uploads"));
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
-
-const upload = multer({ storage });
+const upload = multer({ storage }); 
 
 router.post(
   "/signup",
   upload.single("image"),
-  wrapAsync(usersController.signup)
+  wrapAsync(async (req, res, next) => {
+    try {
+      const { username, email, password } = req.body;
+      const image = req.file ? req.file.path : null; 
+
+      console.log("Uploaded file details:", req.file); 
+      console.log("Image URL:", image); 
+
+      const newUser = new User({
+        email,
+        username,
+        image, 
+      });
+
+      const registeredUser = await User.register(newUser, password);
+
+      req.login(registeredUser, (err) => {
+        if (err) {
+          return next(err);
+        }
+        req.flash("success", "Welcome to Wonderlust");
+        res.redirect("/listings");
+      });
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("/signup");
+    }
+  })
 );
 
 router.get("/signup", usersController.renderSignupform);
