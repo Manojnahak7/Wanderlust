8f240a7b20a73201d8eca6915b5cd387a69cf47c
Manojnahak7 <159554819+Manojnahak7@users.noreply.github.com>
2024-08-30 13:26:08 +0530

    Update cloudConfig.js

--- cloudConfig.js
+++ cloudConfig.js
@@ -11,7 +11,7 @@ const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: "wonderlust_DEV",
-    allowedformat: ["png", "jpg", "jpeg"],
+    allowedFormats: ["png", "jpg", "jpeg"], 
   },
 });
 
