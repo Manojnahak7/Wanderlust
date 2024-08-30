5c6775dbe9d513647bba9cd816cb10a3a676d629
Manojnahak7 <159554819+Manojnahak7@users.noreply.github.com>
2024-08-30 11:30:12 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -5,9 +5,20 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true, 
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true, 
+  },
+  image: {
+    type: String, 
   },
 });
 
-userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(passportLocalMongoose, {
+  usernameField: "email", 
+});
 
 module.exports = mongoose.model("User", userSchema);
